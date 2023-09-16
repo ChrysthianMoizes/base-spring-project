7a29d317ebed48484f2329a2332cef7d40bc7fad
Chrysthian Moizes <chrysthian.moizes@picpay.com>
2023-09-16 15:57:34 -0300

    CreateAccountUseCase

--- src/main/java/com/colatina/app/service/core/domain/AccountDomain.java
+++ src/main/java/com/colatina/app/service/core/domain/AccountDomain.java
@@ -4,6 +4,9 @@ import com.colatina.app.service.core.domain.enumeration.AccountStatus;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -12,13 +15,22 @@ public class AccountDomain {
 
     private Integer id;
 
+    @NotNull
+    @Size(max = 50)
     private String nome;
 
+    @NotNull
+    @Size(max = 100)
     private String lastName;
 
+    @NotNull
+    @Size(min = 11, max = 11)
     private String document;
 
+    @NotNull
     private AccountStatus status;
 
+    @NotNull
+    @Past
     private LocalDate birthDate;
 }
